73cae9e54926a610fd33752f661d4aa82a15418f
QuantumInformatik <juanguillermoalarcon@gmail.com>
2022-10-07 17:49:49 -0500

    agregamos un for

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ console.log("Hola mundo");
 function por(a, b) {
     return a * (-(b));
 }
+
+for (var i = 0; i < 10; i++) {
+    console.log("FOR N");
+}
+
+//****************
+////////////////////////////////////////////////////////////////

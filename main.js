dc288bf3beb01bf3933827cdbddc93a71c260733
berracode <juanguillermoalarcon@gmail.com>
2023-02-09 18:12:27 -0500

    agegué la función de multiplicar dos números

--- main.js
+++ main.js
@@ -16,6 +16,10 @@ function sumar(a, b) {
     return a + b;
 }
 
+function multiplicar(a, b){
+    return a*b;
+}
+
 console.log("Tengo ", sumar(1, 2), " años");
 console.log("Tengo ", sumar(1, 2), " años");
 console.log("Tengo ", sumar(1, 2), " años");

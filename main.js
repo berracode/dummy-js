0efd41649df5af586dc0087cb995ae3f43df39da
QuantumInformatik <juanguillermoalarcon@gmail.com>
2023-02-17 17:25:57 -0500

    se agrega docs

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-console.log("Hola mundo");
+var edades = new Array();
 
-function por(a, b) {
-    return a * (-(b));
+
+function capturar() {
+    var edad = document.getElementById("edad").value
+        //console.log(edad)
+    if (edades.length < 10) {
+        edades.push(edad) // push es un metodo que permite agregar un elemento al arreglo.
+        console.log(edades)
+
+    } else {
+        alert("SOLO SON 10 PERSONAS, IMBECIL!");
+    }
+
+
 }
 
-function sumar(a, b) {
-    return a + b;
-}
 
-console.log("Tengo ", sumar(1, 2), " años");
-console.log("Tengo ", sumar(1, 2), " años");
-console.log("Tengo ", sumar(1, 2), " años");
+function cantidadPersonasMenoresEdad() {
+    var cantidadDePersonasMenoresEdad = 0;
+
+    for (let i = 0; i < edades.length; i++) {
+        if (edades[i] < 18) {
+            cantidadDePersonasMenoresEdad++; // cantidadDePersonasMenoresEdad = cantidadDePersonasMenoresEdad + 1;
+        }
+    }
+
+    document.getElementById("info").innerHTML = "<p> cantidad de personas menores de edad:" + cantidadDePersonasMenoresEdad + " </p>";
+
+}
